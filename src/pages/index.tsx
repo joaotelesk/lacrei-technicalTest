641b7faffe6b162100c336543af381e69ed25d28
Joao Teles <joaojoseteles@gmail.com>
2023-04-22 19:58:12 -0300

    :recycle: refactor: utilizando os componentes criado - ainda em construcaos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 // Components
 import Head from "next/head";
-import { MenuLinks } from "@/components/common/MenuLinks";
+import { MenuLinks } from "@/components/common/MenuLinks/MenuLinks";
 
 // style's
 import { Main } from "@/styles/home";
+import { Title } from "@/components/common/Title/Title";
+import { SubTitle } from "@/components/common/SubTitle/SubTitle";
+import { Button } from "@/components/common/Button/Button";
 
 // Utilities
 // Hooks
@@ -22,8 +25,16 @@ export default function Home() {
       </Head>
 
       <Main>
-        <MenuLinks category="secondary" />
-        <h1>Home | Lacrei Saúde</h1>
+        <section className="flex flex-col gap-y-8">
+          <Title>Boas vindas a Lacrei Saúde</Title>
+          <SubTitle category="primary">
+            Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+
+          </SubTitle>
+          <div className="flex gap-x-16">
+            <Button category="Primary" url="#" text="Pessoa Usuária" />
+            <Button category="Secondary" url="#" text="Profissional" />
+          </div>
+        </section>
       </Main>
     </>
   );
