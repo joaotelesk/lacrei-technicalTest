6a1cbfb07f0377901b79a7792b979e9d6907ea35
Joao Teles <joaojoseteles@gmail.com>
2023-04-21 21:56:26 -0300

    :lipstick: feat: Estilizaçao CSS da pagina Home

---
+++ src/styles/home.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  height: 100vh;
+  width: 100vw;
+  padding: 6rem 4rem;
+  padding-top: 8rem;
+
+  /* tablet */
+  @media (max-width: 768px) {
+    padding: 2rem 2rem;
+    padding-top: 8rem;
+  }
+`;
