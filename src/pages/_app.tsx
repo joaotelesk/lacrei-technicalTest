8b545f330e648aac87642c05334d647f9fbef1c2
Joao Teles <joaojoseteles@gmail.com>
2023-04-21 21:53:35 -0300

    :recycle: refactor: utilizando GlobalStyle e ThemeProvider do styled-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import { Layout } from "@/components/Layout";
-import "@/styles/globals.css";
+import { Layout } from "../components/Layout";
 import type { AppProps } from "next/app";
+import "@/styles/styles.css";
+import { GlobalStyle } from "@/styles/globals";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={{}}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 }
