2b2a2641f3d1d278bc828a252953f185b85665f9
Joao Teles <joaojoseteles@gmail.com>
2023-04-23 22:20:25 -0300

    :recycle: refactor: Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 // Components
 import Head from "next/head";
-import { MenuLinks } from "@/components/common/MenuLinks/MenuLinks";
-
-// style's
-import { Main } from "@/styles/home";
 import { Title } from "@/components/common/Title/Title";
 import { SubTitle } from "@/components/common/SubTitle/SubTitle";
 import { Button } from "@/components/common/Button/Button";
+import Image from "next/image";
+
+// style's
+import {
+  Main,
+  Section,
+  DivContent,
+  DivButtons,
+  DivImg,
+} from "@/styles/StylePages";
 
 // Utilities
 // Hooks
@@ -23,18 +29,27 @@ export default function Home() {
           content="Pessoas LGBTQUA+ nem sempre têm boas experiências ao cuidar da saúde do corpo e da mente. Para mudar esse cenário criamos o Lacrei Saúde."
         />
       </Head>
-
       <Main>
-        <section className="flex flex-col gap-y-8">
-          <Title>Boas vindas a Lacrei Saúde</Title>
-          <SubTitle category="primary">
-            Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+
-          </SubTitle>
-          <div className="flex gap-x-16">
-            <Button category="Primary" url="#" text="Pessoa Usuária" />
-            <Button category="Secondary" url="#" text="Profissional" />
-          </div>
-        </section>
+        <Section>
+          <DivContent>
+            <Title>Boas vindas a Lacrei Saúde</Title>
+            <SubTitle category="primary">
+              Uma plataforma segura e acolhedora para comunidade LGBTQIAPN+
+            </SubTitle>
+            <DivButtons>
+              <Button category="Primary" url="#" text="Pessoa Usuária" />
+              <Button category="Secondary" url="#" text="Profissional" />
+            </DivButtons>
+          </DivContent>
+          <DivImg>
+            <Image
+              src="/Image-Home.svg"
+              width={555}
+              height={421}
+              alt="Imagem da pagina Home"
+            />
+          </DivImg>
+        </Section>
       </Main>
     </>
   );
